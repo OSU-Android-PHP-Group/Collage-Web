52ad4398eb97b9308cf5ea16a89954fe6a6ebb97
Mark Nguyen <pnguyen1097@gmail.com>
2012-05-14 01:35:44 -0400

    Install DoctrineExtensions and StofDoctrineExtensionsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new OSU\CollageBundle\OSUCollageBundle(),
             new OSU\CollageAPIBundle\OSUCollageAPIBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
